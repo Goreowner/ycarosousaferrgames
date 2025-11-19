384246b1139b60c94f7eb1dbb2ce9f736912d7a4
Goreowner <ycarosousaferreira@gmail.com>
2025-11-18 21:55:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Console log for testing
+console.log("Ycaro Sousa Math Games loaded!");
+
+// You can add interactivity here
+// Example: Add a simple math alert
+function playMathGame(game) {
+  alert(`${game} is coming soon!`);
+}
